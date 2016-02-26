c87f5e9c662654fb615653ad929774738def91ff
Filip Chyla <philipchyla@gmail.com>
2016-02-26 16:50:55 +0100

    created wfuzz install module

---
+++ modules/intelligence-gathering/wfuzz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#######################################
+# Installation module for Wfuzz
+#######################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Filp Chyla (keresh)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Wfuzz - The Web Bruteforcer"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/xmendez/wfuzz.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="wfuzz"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN=""
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
